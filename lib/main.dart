4309b1b2a6205c4145ccf5c8de9f3b95f034ce34
gerges46 <145163994+gerges46@users.noreply.github.com>
2024-11-24 20:48:09 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SplashView());
+}
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
