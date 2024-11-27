0058a67527a73b2f8a2fd27dfb5f78ddb2651b06
gerges46 <145163994+gerges46@users.noreply.github.com>
2024-11-27 23:59:16 +0200

    create style file & add title to home view

--- lib/features/home/data/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/data/presentation/views/widgets/custom_list_view_item.dart
@@ -1,8 +1,8 @@
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,38 +1,27 @@
+import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
-
 import 'custom_appBar.dart';
-import 'custom_list_view_item.dart';
+import 'featured_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-
-        FeaturedBooksListView(),
-      ],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListViewItem(),
-            );
-          }),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(height: 50,),
+          Text(
+            "best Saller",
+            style: Styles.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
