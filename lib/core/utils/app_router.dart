872ed8375cdc0c162c7b25d0deab619524053fd8
gerges46 <145163994+gerges46@users.noreply.github.com>
2024-12-03 09:22:24 +0200

    refactor navigation

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/data/presentation/views/home_view.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../features/splash/presentation/views/splash_view.dart';
+
+abstract class AppRouter {
+  static const kHomeView="/HomeView";
+ static final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const SplashView(),
+    ),
+    GoRoute(
+      path: kHomeView,
+      builder: (context, state) => const HomeView(),
+    ),
+  ],
+);
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/features/home/data/presentation/views/home_view.dart';
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 import 'sliding_text.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -55,12 +55,14 @@ class _SplashViewBodyState extends State<SplashViewBody>
             .animate(animationController);
     animationController.forward();
   }
-}
-
-
   void navigateToHome() {
     Future.delayed(const Duration(seconds: 3), () {
-      Get.to(const HomeView(),
-          transition: Transition.fade, duration: kTransitionDuration);
+    /*   Get.to(const HomeView(),
+          transition: Transition.fade, duration: kTransitionDuration); */
+  
+        GoRouter.of(context).push(AppRouter.kHomeView);
+     
     });
   }
+}
+
