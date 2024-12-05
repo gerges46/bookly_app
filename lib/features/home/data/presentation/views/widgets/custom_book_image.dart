03821f9d86925135f0013e69c8dc6facf47c90b2
gerges46 <145163994+gerges46@users.noreply.github.com>
2024-12-05 23:36:44 +0200

    ues cached network image

--- lib/features/home/data/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/data/presentation/views/widgets/custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -6,18 +7,17 @@ class CustomBookImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: Colors.red,
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(
-              urlImage,
-            ),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child:
+       
+            CachedNetworkImage(
+              fit: BoxFit.fill,
+              
+          imageUrl: urlImage,
+              errorWidget: (context, url, error) => const Icon(Icons.error,size: 80,color: Colors.red,),
         ),
       ),
     );
--- lib/features/home/data/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/data/presentation/views/widgets/featured_list_view.dart
@@ -16,8 +16,8 @@ class FeaturedBooksListView extends StatelessWidget {
           return SizedBox(
             height: MediaQuery.of(context).size.height * 0.3,
             child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: state.books.length,
+                physics: const BouncingScrollPhysics(),
+                itemCount: state.books.length,
                 scrollDirection: Axis.horizontal,
                 itemBuilder: (context, index) {
                   return Padding(
