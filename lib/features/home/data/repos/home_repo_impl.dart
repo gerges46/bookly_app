88de2af6e8ee106c5a23846de69e29ba7f7a6791
gerges46 <145163994+gerges46@users.noreply.github.com>
2024-12-09 09:51:24 +0200

    implement similar books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -51,4 +51,22 @@ class HomeRepoImpl implements HomeRepo {
       return left(ServerFailure(e.toString()));
     }
   }
+  
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks({required String category}) async{
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Sorting=relevance&Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioErorr(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
+  }
 }
